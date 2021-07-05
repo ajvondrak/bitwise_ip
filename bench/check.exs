0be547520bd82132cddf0258dcd89e3c734b7ccd
Alex Vondrak <ajvondrak@gmail.com>
2021-07-04 18:00:30 -0700

    Copy benchmarks over from RemoteIp
    The early results are actually not at all promising. I guess BitwiseIp
    parsing is about on par with the others, which isn't all that
    surprising:
    
      bitwise_ip        307.72
      cider             307.58 - 1.00x slower +0.00144 ms
      remote_ip         294.73 - 1.04x slower +0.143 ms
      inet_cidr         269.15 - 1.14x slower +0.47 ms
      cidr              199.39 - 1.54x slower +1.77 ms
    
    But more importantly, the membership checks are consistently much slower
    than RemoteIp and Cider:
    
      remote_ip          15.42
      cider              11.52 - 1.34x slower +21.92 ms
      bitwise_ip         10.79 - 1.43x slower +27.82 ms
      inet_cidr           7.00 - 2.20x slower +77.94 ms
      cidr                1.67 - 9.26x slower +535.33 ms

---
+++ bench/check.exs
@@ -0,0 +1,56 @@
+Bench.Inputs.seed
+
+ips = Bench.Inputs.ips(1_000)
+
+parsed_ips = %{
+  bitwise_ip: Enum.map(ips, &BitwiseIp.encode/1),
+  remote_ip: Enum.map(ips, &RemoteIp.Block.encode/1),
+  inet_cidr: ips,
+  cider: Enum.map(ips, &Cider.ip!/1),
+  cidr: ips,
+}
+
+cidrs = Bench.Inputs.cidrs(1_000)
+
+parsed_cidrs = %{
+  bitwise_ip: Enum.map(cidrs, &BitwiseIp.Block.parse!/1),
+  remote_ip: Enum.map(cidrs, &RemoteIp.Block.parse!/1),
+  inet_cidr: Enum.map(cidrs, &InetCidr.parse(&1, true)),
+  cider: Enum.map(cidrs, &Cider.parse/1),
+  cidr: Enum.map(cidrs, &CIDR.parse/1),
+}
+
+suite = %{
+  bitwise_ip: fn ->
+    Enum.each(parsed_ips[:bitwise_ip], fn ip ->
+      Enum.each(parsed_cidrs[:bitwise_ip], &BitwiseIp.Block.member?(&1, ip))
+    end)
+  end,
+  remote_ip: fn ->
+    Enum.each(parsed_ips[:remote_ip], fn ip ->
+      Enum.each(parsed_cidrs[:remote_ip], &RemoteIp.Block.contains?(&1, ip))
+    end)
+  end,
+  inet_cidr: fn ->
+    Enum.each(parsed_ips[:inet_cidr], fn ip ->
+      Enum.each(parsed_cidrs[:inet_cidr], &InetCidr.contains?(&1, ip))
+    end)
+  end,
+  cider: fn ->
+    Enum.each(parsed_ips[:cider], fn ip ->
+      Enum.each(parsed_cidrs[:cider], &Cider.contains?(ip, &1))
+    end)
+  end,
+  cidr: fn ->
+    Enum.each(parsed_ips[:cidr], fn ip ->
+      Enum.each(parsed_cidrs[:cidr], &CIDR.match(&1, ip))
+    end)
+  end,
+}
+
+formatters = [
+  {Benchee.Formatters.HTML, file: "tmp/check.html", auto_open: false},
+  Benchee.Formatters.Console,
+]
+
+Benchee.run(suite, formatters: formatters)
