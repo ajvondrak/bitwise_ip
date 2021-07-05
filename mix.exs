f164f2fcc8a1282f766f1daba50943d3d541e0a4
Alex Vondrak <ajvondrak@gmail.com>
2021-07-05 16:19:39 -0700

    Flesh out package information for Hex.pm

--- mix.exs
+++ mix.exs
@@ -5,7 +5,13 @@ defmodule BitwiseIp.MixProject do
     [
       app: :bitwise_ip,
       version: "0.1.0",
-      elixir: "~> 1.7"
+      elixir: "~> 1.7",
+      description: "Efficient IP address operations using bitwise arithmetic",
+      package: %{
+        files: ~w[lib mix.exs README.md LICENSE],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/ajvondrak/bitwise_ip"}
+      }
     ]
   end
 
