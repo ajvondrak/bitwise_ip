de3530ae97c9fe4580e81c4a114374324506d759
Alex Vondrak <ajvondrak@gmail.com>
2021-07-20 14:26:22 -0700

    Benchmark BitwiseIp.Mask
    This compares the compile-time pattern matching approach to the run-time
    bitwise math. Unlike the BitwiseIp.Block.size/1 pattern matching on
    structs, it seems that looking up integer values in function clauses is
    much faster than doing the binary manipulation at run time.
    
      encode_compile      366.73 K
      encode_run           75.32 K - 4.87x slower +10.55 μs
    
      decode_compile       20.78 K
      decode_run            5.63 K - 3.69x slower +129.42 μs
    
      parse_compile      123.49 K
      parse_run           54.45 K - 2.27x slower +10.27 μs

---
+++ bench/mask.exs
@@ -0,0 +1,103 @@
+defmodule Compile do
+  use Bitwise
+
+  @v4 0xFFFFFFFF
+
+  for decoded <- 0..32 do
+    <<encoded::32>> = <<(~~~(@v4 >>> decoded))::32>>
+    def encode(:v4, unquote(decoded)), do: unquote(encoded)
+    def decode(:v4, unquote(encoded)), do: unquote(decoded)
+    def parse(:v4, unquote(to_string(decoded))), do: {:ok, unquote(encoded)}
+  end
+
+  @v6 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+  for decoded <- 0..128 do
+    <<encoded::128>> = <<(~~~(@v6 >>> decoded))::128>>
+    def encode(:v6, unquote(decoded)), do: unquote(encoded)
+    def decode(:v6, unquote(encoded)), do: unquote(decoded)
+    def parse(:v6, unquote(to_string(decoded))), do: {:ok, unquote(encoded)}
+  end
+
+  def parse(:v4, prefix) do
+    {:error, "Invalid IPv4 mask #{inspect(prefix)}"}
+  end
+
+  def parse(:v6, prefix) do
+    {:error, "Invalid IPv6 mask #{inspect(prefix)}"}
+  end
+end
+
+defmodule Run do
+  use Bitwise
+
+  @v4 0xFFFFFFFF
+  @v6 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+  def encode(:v4, decoded) do
+    <<encoded::32>> = <<(~~~(@v4 >>> decoded))::32>>
+    encoded
+  end
+
+  def encode(:v6, decoded) do
+    <<encoded::128>> = <<(~~~(@v6 >>> decoded))::128>>
+    encoded
+  end
+
+  def decode(_proto, mask) do
+    ones = for <<1::1 <- :binary.encode_unsigned(mask)>>, do: 1
+    length(ones)
+  end
+
+  def parse(proto, prefix) do
+    encode(proto, String.to_integer(prefix))
+  end
+end
+
+v4 = Enum.to_list(0..32)
+v6 = Enum.to_list(0..128)
+
+suite = %{
+  encode_run: fn ->
+    Enum.each(v4, &Run.encode(:v4, &1))
+    Enum.each(v6, &Run.encode(:v6, &1))
+  end,
+  encode_compile: fn ->
+    Enum.each(v4, &Compile.encode(:v4, &1))
+    Enum.each(v6, &Compile.encode(:v6, &1))
+  end,
+}
+
+Benchee.run(suite)
+
+v4 = for mask <- 0..32, do: BitwiseIp.Mask.encode(:v4, mask)
+v6 = for mask <- 0..128, do: BitwiseIp.Mask.encode(:v6, mask)
+
+suite = %{
+  decode_run: fn ->
+    Enum.each(v4, &Run.decode(:v4, &1))
+    Enum.each(v6, &Run.decode(:v6, &1))
+  end,
+  decode_compile: fn ->
+    Enum.each(v4, &Compile.decode(:v4, &1))
+    Enum.each(v6, &Compile.decode(:v6, &1))
+  end,
+}
+
+Benchee.run(suite)
+
+v4 = for mask <- 0..32, do: Integer.to_string(mask)
+v6 = for mask <- 0..128, do: Integer.to_string(mask)
+
+suite = %{
+  parse_run: fn ->
+    Enum.each(v4, &Run.parse(:v4, &1))
+    Enum.each(v6, &Run.parse(:v6, &1))
+  end,
+  parse_compile: fn ->
+    Enum.each(v4, &Compile.parse(:v4, &1))
+    Enum.each(v6, &Compile.parse(:v6, &1))
+  end,
+}
+
+Benchee.run(suite)
