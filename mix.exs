d8cd6cc1f1541e6c8e65173046352b6286fd40b4
Alex Vondrak <ajvondrak@gmail.com>
2024-01-07 12:13:10 -0800

    Prepare v1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BitwiseIp.MixProject do
   def project do
     [
       app: :bitwise_ip,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       description: "Efficient IP address operations using bitwise arithmetic",
       package: %{
